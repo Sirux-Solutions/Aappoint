714d4e66a370326c1ce15b8f8489ca1e6c30d65b
Sirasit Thitirattanakorn <sirasit@siruxsolutions.com>
2023-12-20 02:11:48 -0800

    added services section

--- src/components/layouts/home01/Feature2.jsx
+++ src/components/layouts/home01/Feature2.jsx
@@ -8,41 +8,47 @@ class Feature2 extends Component {
                 {
                     id: 1,
                     icon: 'bi bi-badge2',
-                    title: 'Entrepreneurs',
-                    text: 'Behind the word mountains, far from the countries'
+                    title: 'ระบบคิวจัดการผ่านออนไลน์',
+                    text: ''
                 },
                 {
                     id: 2,
                     icon: 'bi bi-article',
-                    title: 'Project Managers',
-                    text: 'Behind the word mountains, far from the countries'
+                    title: 'เพิ่มปุ่ม “จองโต๊ะ” บน Google Maps สำหรับธุรกิจร้านอาหาร',
+                    text: ''
                 },
                 {
                     id: 3,
                     icon: 'bi bi-headset',
-                    title: 'Students',
-                    text: 'Behind the word mountains, far from the countries'
-                }
+                    title: 'ลูกค้าจองคิวผ่าน แอพพลิเคชั่น AAppoint',
+                    text: ''
+                },
+                {
+                    id: 4,
+                    icon: 'bi bi-headset',
+                    title: 'แจ้งเตือนนัดหมาย ผ่าน SMS และ E-mail',
+                    text: ''
+                },
             ]
         }
     }
 
     render() {
         return (
-            <section className="feature-2 section-padding ">
+            <section className="feature-2 section-padding " id="services">
                 <div className="container">
                     <div className="row justify-content-center">
                         <div className="col-lg-6">
                             <div className="section-heading center-heading text-center mb-60">
-                                <h3 className="heading-title">Who This Book Is For</h3>
-                                <p>This book is concerned with creating typography and is essential for professionals who regularly work for clients.</p>
+                                <h3 className="heading-title">บริการของเรา</h3>
+                                <p>บริการของเรา ช่วยจัดการตารางานนัดหมายของธุรกิจ เพื่อสร้างความประทับใจให้กับลูกค้าของคุณ</p>
                             </div>
                         </div>
                     </div>
                     <div className="row ">
                         {
                             this.state.features.map((data,index) => (
-                                <div className="col-lg-4 col-md-6">
+                                <div className="col-lg-3 col-md-4">
                                     <div className="feature-style-2 mb-4 mb-lg-0" key={data.id}>
                                         <i className={data.icon}></i>
                                         <div className="feature-text">
