e26e219cc382501d5588f6d5027ec00e6a43342d
Sirasit Thitirattanakorn <sirasit@siruxsolutions.com>
2023-12-19 22:44:02 -0800

    finished draft: banner

--- src/components/layouts/home01/Banner.jsx
+++ src/components/layouts/home01/Banner.jsx
@@ -8,23 +8,13 @@ class Banner extends Component {
             listItem: [
                 {
                     id: 1,
-                    icon: 'fa fa-check',
-                    title: 'Learn From Expert in details',
+                    icon: 'fa fa-business-time',
+                    title: 'We make your business better.',
                 },
                 {
                     id: 2,
                     icon: 'fa fa-check',
-                    title: '16 Chapters included',
-                },
-                {
-                    id: 3,
-                    icon: 'fa fa-check',
-                    title: 'eBook includes iBooks, PDF & ePub versions',
-                },
-                {
-                    id: 4,
-                    icon: 'fa fa-check',
-                    title: 'Audio book with every purchase',
+                    title: 'ระบบจองคิว เพื่อธุรกิจ',
                 },
             ]
         }
@@ -34,10 +24,15 @@ class Banner extends Component {
             <section className="banner pb-80">
                 <div className="container">
                     <div className="row align-items-center justify-content-between">
+                    <div className="col-lg-5">
+                        <div className="banner-img">
+                                <img src="assets/images/banner/banner.jpg" alt="" className="img-fluid"/>
+                            </div>
+                        </div>
                         <div className="col-md-12 col-lg-6">
                             <div className="banner-content ">
-                                <span className="subheading">50% discount on early purchase</span>
-                                <h1>Solve your daily life problem in 1 minute</h1>
+                                {/* <span className="subheading"></span> */}
+                                <h1>AAppoint</h1>
                                 <ul>
                                     {
                                     this.state.listItem.map((data,i) => (
@@ -45,15 +40,10 @@ class Banner extends Component {
                                         ))
                                     }
                                 </ul>
-                                <Link to="#" className="btn btn-main">Get this book at 29$</Link>  
-                                <p>Interested in free chapter? <Link to="#">Get it now</Link></p>
+                                <Link to="#" className="btn btn-main">เปิดร้านค้ากับ AAppoint</Link>  
                             </div>
                         </div>
-                        <div className="col-lg-5">
-                            <div className="banner-img">
-                                <img src="assets/images/banner/book.png" alt="" className="img-fluid"/>
-                            </div>
-                        </div>
+                        
                     </div> 
                 </div> 
             </section>
