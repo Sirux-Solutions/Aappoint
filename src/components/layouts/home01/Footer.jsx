7f3a1487c48d040524ee1837264583e0079cee50
Sirasit Thitirattanakorn <sirasit@siruxsolutions.com>
2023-12-22 14:39:53 -0800

    finished draft for footer

--- src/components/layouts/home01/Footer.jsx
+++ src/components/layouts/home01/Footer.jsx
@@ -4,54 +4,27 @@ import { Link } from 'react-router-dom';
 class Footer extends Component {
     render() {
         return (          
-            <section className="footer pt-120">
-                <div className="container">
-                    <div className="row">
-                        <div className="col-lg-4 me-auto col-sm-6 col-md-12">
-                            <div className="widget footer-widget mb-5 mb-lg-0">
-                                <h5 className="widget-title">BookHunt</h5>
-                                <p className="mt-3">Veniam Sequi molestias aut necessitatibus optio magni at natus accusamus. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Cupiditate, obcaecat .</p>
-                            </div>
-                        </div>
-                        
-                        <div className="col-lg-6 col-sm-6 col-md-12">
-                            <div className="footer-widget mb-5 mb-lg-0">
-                                <h5 className="widget-title">Information</h5>
-                            </div>
-                            <div className="row">
-                                <div className="col-lg-5 col-md-6">
-                                    <ul className="list-unstyled footer-links">
-                                        <li><Link to="#">SEO Business</Link></li>
-                                        <li><Link to="#">Digital Marketing</Link></li>
-                                        <li><Link to="#">Graphic Design</Link></li>
-                                        <li><Link to="#">Site Development</Link></li>
-                                    </ul>
-                                </div>
-                                <div className="col-lg-6 col-md-6">
-                                    <div className="footer-widget footer-contact mb-5 mb-lg-0">
-                                        <ul className="list-unstyled">
-                                            <li> +1 (305) 547-9909</li>
-                                            <li>382 NE 191st St # 87394 Miami, FL 33179-3899</li>
-                                            <li>info@example.com</li>
-                                        </ul>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+            <section className="footer">
+                
 
-                <div className="footer-btm">
                     <div className="container">
                         <div className="row">
-                            <div className="col-lg-6">
-                                <div className="copyright">
-                                    <p>@ 2023-Copyright reserved to BooHunt.Proudly Crafted by <Link to="https://themeforest.net/user/pxelcode">Pxelcode</Link> </p>
+                            <div className="col-lg-12">
+                            <div className="header-right-info text-center">
+                                <ul className="header-socials">
+                                    <li><Link to="#"><i className="fab fa-facebook-f" aria-hidden="true" /></Link></li>
+                                    <li><Link to="#"><i className="fab fa-twitter" aria-hidden="true" /></Link></li>
+                                    <li><Link to="#"><i className="fab fa-linkedin" aria-hidden="true" /></Link></li>
+                                    <li><Link to="#"><i className="fab fa-pinterest" aria-hidden="true" /></Link></li>
+                                </ul>
+                            </div>
+                            
+                                <div className="copyright text-center">
+                                    <p>@ 2024-Copyright reserved to AAppoint. Proudly Crafted by <Link to="https://www.siruxsolutions.com/">SIRUX</Link> </p>
                                 </div>
                             </div>
                         </div>
                     </div>
-                </div>
             
                 <div className="fixed-btm-top">
                     <a href="#top-header" className="js-scroll-trigger scroll-to-top"><i className="fa fa-angle-up"></i></a>
