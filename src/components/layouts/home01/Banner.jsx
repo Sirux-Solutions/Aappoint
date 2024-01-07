f6e34f4803a75c44363f9c4bfb404669bc8c6991
Sirasit Thitirattanakorn <sirasit@siruxsolutions.com>
2024-01-07 11:47:50 -0800

    updated banner page

--- src/components/layouts/home01/Banner.jsx
+++ src/components/layouts/home01/Banner.jsx
@@ -4,45 +4,50 @@ import { Link } from 'react-router-dom';
 class Banner extends Component {
     constructor(props){
         super(props);
-        this.state = {
-            listItem: [
-                {
-                    id: 1,
-                    icon: 'fa fa-business-time',
-                    title: 'We make your business better.',
-                },
-                {
-                    id: 2,
-                    icon: 'fa fa-check',
-                    title: 'ระบบจองคิว เพื่อธุรกิจ',
-                },
-            ]
-        }
+        // this.state = {
+        //     listItem: [
+        //         {
+        //             id: 1,
+        //             icon: 'fa fa-business-time',
+        //             title: 'เราช่วยดึงนักท่องเที่ยวมาใช้บริการให้ร้านอาหารของคุณ',
+        //         },
+        //         {
+        //             id: 2,
+        //             icon: 'fa fa-check',
+        //             title: 'ระบบจองคิว เพื่อธุรกิจ',
+        //         },
+        //     ]
+        // }
     }
     render() {
         return (
             <section className="banner pb-80">
                 <div className="container">
                     <div className="row align-items-center justify-content-between">
-                    <div className="col-lg-6">
-                        <div className="banner-img">
-                                <img src="assets/images/banner/banner.jpg" alt="" className="img-fluid"/>
-                            </div>
+                        <div className='col-lg-1'>
+
                         </div>
                         <div className="col-md-12 col-lg-5">
-                            <div className="banner-content ">
+                            <div className="banner-content">
                                 {/* <span className="subheading"></span> */}
-                                <h1>AAppoint</h1>
-                                <ul>
+                                <h1>เพิ่มปุ่ม จองโต๊ะ</h1>
+                                <h1>ผ่าน Google Maps</h1>
+                                <p>เราช่วยดึงนักท่องเที่ยวมาใช้บริการให้ร้านอาหารของคุณ</p>
+                                {/* <ul>
                                     {
                                     this.state.listItem.map((data,i) => (
                                         <li key={i}><i className={data.icon}></i> {data.title}</li>
                                         ))
                                     }
-                                </ul>
-                                <Link to="#" className="btn btn-main">เปิดร้านค้ากับ AAppoint</Link>  
+                                </ul> */}
+                                <Link to="#" className="btn btn-main">เปิดใช้งานทันที</Link>  
                             </div>
                         </div>  
+                        <div className="col-lg-6">
+                            <div className="banner-img">
+                                <img src="assets/images/banner/banner.png" alt="" className="img-fluid"/>
+                            </div>
+                        </div>
                     </div> 
                 </div> 
             </section>
