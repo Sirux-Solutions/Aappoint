57c9bb0c3c3d43aeb4255808508a396c72ab2387
Sirasit Thitirattanakorn <sirasit@siruxsolutions.com>
2024-03-23 00:01:25 -0700

    updated meta data

--- src/components/pages/Home01.jsx
+++ src/components/pages/Home01.jsx
@@ -8,8 +8,8 @@ class Home01 extends Component {
             <div className="counter-scroll bg-home1 has-one-page">
                 <Helmet>
                     {/* Meta tags go here */}
-                    <meta name="description" content="Google Maps, ค้นหาใน Google, จองโต๊ะ, จองร้านอาหาร, SEO, ล่วงหน้า, นักท่องเที่ยว, นักท่องเที่ยวต่างชาติ, ร้านอาหารไทย, ธุรกิจร้านอาหาร, กำไร, การตลาดออนไลน์, คู่แข่ง, POS, ลูกค้า, ยอดขาย, ประหยัดต้นทุน, เปิดร้านอาหาร, การจัดการ, ใกล้ฉัน, จองคิว, ระบบร้านอาหาร, ปักหมุด, โพสต์รีวิว, พี่จอง, คัลเลน, คัลแลน, การจอง, นัดหมาย, สำรอง, สำรองที่นั่ง, สำรองโต๊ะ, คิวยาว, ไม่มีคิว" />
-                    <meta name="description" content="Google Maps, Google search, Table booking/reservation, Restaurant booking/reservation, SEO, In advance, Tourist, Foreign tourist, Thai Restaurant, Restaurant business, Profit, Online marketing, Competitor, POS, Customer, Sales, Save cost, Open a restuarant, Management, Near me, Make a reservation, Restaurant system, Pin location, Review post, Cullen Hateberry, Appointment, Waiting" />
+                    <meta name="keywords" content="Aappoint, Google Maps, ค้นหาใน Google, จองโต๊ะ, จองร้านอาหาร, SEO, ล่วงหน้า, นักท่องเที่ยว, นักท่องเที่ยวต่างชาติ, ร้านอาหารไทย, ธุรกิจร้านอาหาร, กำไร, การตลาดออนไลน์, คู่แข่ง, POS, ลูกค้า, ยอดขาย, ประหยัดต้นทุน, เปิดร้านอาหาร, การจัดการ, ใกล้ฉัน, จองคิว, ระบบร้านอาหาร, ปักหมุด, โพสต์รีวิว, พี่จอง, คัลเลน, คัลแลน, การจอง, นัดหมาย, สำรอง, สำรองที่นั่ง, สำรองโต๊ะ, คิวยาว, ไม่มีคิว" />
+                    <meta name="keywords" content="Aappoint, Google Maps, Google search, Table booking/reservation, Restaurant booking/reservation, SEO, In advance, Tourist, Foreign tourist, Thai Restaurant, Restaurant business, Profit, Online marketing, Competitor, POS, Customer, Sales, Save cost, Open a restuarant, Management, Near me, Make a reservation, Restaurant system, Pin location, Review post, Cullen Hateberry, Appointment, Waiting" />
                 </Helmet>
 
                 {/* Uncomment if you have a Header component */}
